414eb235d8ac4faf95b53e3ce0813c0a22a20117
Jian yong Lei <jianyonglei@Jians-MacBook-Pro.local>
2022-12-08 20:34:58 -0500

    backend init

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const PORT = process.env.PORT || 3080
+
+
+app.use(express.json())
+
+app.use(cookieParser())
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.use('/', require('./routes/root'))
+
+app.all('*', (req, res) => {
+    res.status(400)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({ message: '404 Not Found' })
+    } else {
+        res.type('text').send('404 Not Found')
+    }
+})
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
