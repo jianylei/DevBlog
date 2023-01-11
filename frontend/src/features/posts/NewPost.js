1b2fcf7fd9659a059a9a21e886e77af146400b68
Jian yong Lei <jianyonglei@gmail.com>
2023-01-11 00:01:54 -0500

    added side labels for form input

--- frontend/src/features/posts/NewPost.js
+++ frontend/src/features/posts/NewPost.js
@@ -1,37 +1,82 @@
 import { useEffect, useState } from "react"
 import { Editor } from "@tinymce/tinymce-react"
+import {
+    stringToTags,
+    parseImgFromHTML,
+    dataURLtoFile
+} from "../../utils/postFormUtils"
+import { useUploadMutation } from "../uploads/uploadApiSlice"
 
 const NewPost = () => {
     const [title, setTitle] = useState('')
     const [subhead, setSubhead] = useState('')
     const [content, setContent] = useState('')
     const [tags, setTags] = useState('')
+    const [cover , setCover] = useState('')
 
+    const [upload, {
+      isError,
+      error
+    }] = useUploadMutation()
+
+    const handleSubmit = async (e) => {
+      e.preventDefault()
+
+      const tagsList = stringToTags(tags)
+      const { str, imageList, imageNames } = parseImgFromHTML(content)
+      const data = new FormData()
+      data.append('test', 'tesetset')
+      data.append('name', 'jianyonglei')
+
+      for (const idx in imageList) {
+        const file = dataURLtoFile(imageList[idx], imageNames[idx])
+        console.log(file)
+        data.append('posts', file)
+      }        data.append('users', 'file')
+
+      await upload(data)
+    }
+
+    const onImageChange = (e) => {
+      if (e.target.files && e.target.files[0]) {
+        setCover(URL.createObjectURL(e.target.files[0]))
+      }
+    }
+
+    const coverImg = cover
+      ? `url(${cover})`
+      : 'var(--NO-IMAGE)'
 
     return (
         <div className="form__container">
-            <form className="form" onSubmit={e => e.preventDefault()}>
+            <form className="form" onSubmit={handleSubmit}>
+              <div className="form-input__container">
                 <input
-                    className="form__input"
-                    id="title"
-                    name="title"
-                    type="text"
-                    placeholder="Title"
-                    autoComplete="off"
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                />
-                <input
-                    className="form__input"
-                    id="subheading"
-                    name="subheading"
-                    type="text"
-                    placeholder="Subheading"
-                    autoComplete="off"
-                    value={subhead}
-                    onChange={(e) => setSubhead(e.target.value)}
-                />
-                <div className="form-tags__container">
+                      className="form__input"
+                      id="title"
+                      name="title"
+                      type="text"
+                      placeholder="Title"
+                      autoComplete="off"
+                      value={title}
+                      onChange={(e) => setTitle(e.target.value)}
+                  />
+                   <span className="title-span"></span>
+              </div>
+                <div className="form-input__container">
+                  <input
+                      className="form__input"
+                      id="subheading"
+                      name="subheading"
+                      type="text"
+                      placeholder="Subheading"
+                      autoComplete="off"
+                      value={subhead}
+                      onChange={(e) => setSubhead(e.target.value)}
+                  />
+                  <span className="sub-span"></span>
+                </div>
+                <div className="form-input__container">
                   <input
                       className="form__input"
                       id="tags"
@@ -42,21 +87,30 @@ const NewPost = () => {
                       value={tags}
                       onChange={(e) => setTags(e.target.value)}
                   />
-                  <p className="form-tags-note">*Tags are space (and) or comma seperated</p>
+                  <span className="tags-span"></span>
+                  <p className="form-tags-note">*Tags are space and/or comma seperated</p>
                 </div>
+                <label className="form-cover__container" htmlFor="cover">
+                    <input id='cover' type='file' accept="image/*" onChange={onImageChange} hidden/>
+                    <div
+                        className={`image post-card-cover ${cover ? 'img-overlay' : ''}`}
+                        style={{backgroundImage: coverImg}}
+                    />
+                </label>
                 <div className="form__textarea">
                     <Editor apiKey='fo5qm0cg8ib5w52ryt8bkwt18xm5lwwjj90gxlu7q1zh9ir6'
                         init={{
-                          plugins: 'link image',
-                          toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | h1 h2 h3',
-                          placeholder: 'Tell your story...',
+                          plugins: 'link image lists',
+                          toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | numlist bullist | h1 h2 h3',
                           statusbar: false,
                           height:"100%",
-                          menubar: true
+                          content_style: "@import url('https://fonts.googleapis.com/css2?family=Noto+Serif+Toto" + 
+                            "&display=swap'); body { font-family: 'Noto Serif Toto', sans-serif; }",
+                          placeholder: 'Tell your story...',
                         }}
                         onChange={(e) => setContent(e.target.getContent())}
                     />
-                </div>
+                </div><button type="submit">asdasd</button>
             </form>
         </div>
     )
---
+++ frontend/src/utils/postFormUtils.js
@@ -0,0 +1,43 @@
+import { IMGPATH } from "../constants/constants"
+
+
+export const stringToTags = (str) => {
+    const tags = str.toLowerCase().split(/\s?[, ]\s?/)
+    return tags.filter(n => n)
+}
+
+export const dataURLtoFile = (dataurl, filename) => {
+    var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+    bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+    while(n--){
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new File([u8arr], filename, {type:mime});
+}
+
+export const parseImgFromHTML = (str) => {
+    const imageList = []
+    const imageNames = []
+    let newStr = ''
+
+    const subStr = str.split('src="')
+
+    newStr += subStr[0]
+
+    for (let i = 1; i < subStr.length; i++) {
+        const before = subStr[i].slice(0, subStr[i].indexOf('"'))
+        const after = subStr[i].slice(subStr[i].indexOf('"') + 1)
+        const name = Date.now() + '-' + Math.round(Math.random() * 1E9) + '.jpg'
+
+        imageList.push(before)
+        imageNames.push(name)
+
+        newStr += 'src="' + IMGPATH.Images + name + '"' + after
+    }
+
+    return ({
+        str: newStr,
+        imageList,
+        imageNames
+    })
+}
