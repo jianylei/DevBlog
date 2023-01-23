3d3c9a7949590bcb7454d200b65b21e663fdf577
Jian yong Lei <jianyonglei@gmail.com>
2023-01-22 20:19:34 -0500

    created Header.js from Layout.js

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,92 @@
+import { useEffect, useState } from "react"
+import { Outlet, Link, useNavigate, useLocation } from 'react-router-dom'
+import { useDispatch, useSelector } from "react-redux"
+import { useSendLogoutMutation } from '../features/auth/authApiSlice'
+import { setOpen, setType } from "../features/modal/modalSlice"
+import useAuth from '../hooks/useAuth'
+import Modal from '../features/modal/Modal'
+import { MODAL, REGEX } from '../constants/constants'
+import PublishButton from "../features/posts/PublishButton"
+
+const Header = ({ showState }) => {
+    const [show, setShow] = showState
+    const [lastScrollY, setLastScrollY] = useState(0)
+  
+    const { username, role } = useAuth()
+
+    const dispatch = useDispatch()
+
+    const navigate = useNavigate()
+
+    const { pathname } = useLocation()
+
+    const [sendLogout, {
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    }] = useSendLogoutMutation()
+
+    useEffect(() => {
+        const controlNavbar = () => {
+            if (typeof window !== 'undefined') { 
+              if (window.scrollY > lastScrollY) { // if scroll down hide the navbar
+                setShow(false)
+              } else { // if scroll up show the navbar
+                if (window.scrollY > 104) {
+                  setShow(true)
+                }
+              }
+              setLastScrollY(window.scrollY)
+            }
+          }
+  
+        if (typeof window !== 'undefined') {
+            window.addEventListener('scroll', controlNavbar)
+
+            return () => {
+                window.removeEventListener('scroll', controlNavbar)
+            }
+        }
+    }, [lastScrollY])
+
+    const clickHandle = (t) => {
+        dispatch(setType({ type: t }))
+        dispatch(setOpen({ open: true }))
+    }
+
+    const navRight = () => {
+        if (!role) {
+            return (
+                <nav className="main-header__nav">
+                    <button className="login__button" onClick={() => clickHandle(MODAL.TYPE.SignIn)}>
+                        Sign In</button>
+                    <button className="signup__button" onClick={() => clickHandle(MODAL.TYPE.SignUp)}>
+                        Sign Up</button>
+                </nav>
+            )
+        }
+
+        return (
+            <nav className="main-header__nav">
+                { REGEX.ROUTES.WRITE.test(pathname)
+                    ? <PublishButton />
+                    : <button className="login__button" onClick={() => navigate('/write')}>Write</button>
+                }
+                <button className="login__button" onClick={sendLogout}>Log off</button>
+            </nav>
+        )
+    }
+
+    return (
+        <header className={`main-header__container ${show 
+            && 'main-header-scroll'}`}>
+            <Link to='/'>
+                <h1 className="main-header__title">DevSpot</h1>
+            </Link>
+            {navRight()}
+        </header>
+    )
+}
+
+export default Header
--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -7,87 +7,15 @@ import useAuth from '../hooks/useAuth'
 import Modal from '../features/modal/Modal'
 import { MODAL, REGEX } from '../constants/constants'
 import PublishButton from "../features/posts/PublishButton"
+import Header from "./Header"
 
 const Layout = () => {
     const [show, setShow] = useState(false)
-    const [lastScrollY, setLastScrollY] = useState(0)
-  
-    const { username, role } = useAuth()
-
-    const dispatch = useDispatch()
-
-    const navigate = useNavigate()
-
-    const { pathname } = useLocation()
-
-    const [sendLogout, {
-        isLoading,
-        isSuccess,
-        isError,
-        error
-    }] = useSendLogoutMutation()
-
-    useEffect(() => {
-        const controlNavbar = () => {
-            if (typeof window !== 'undefined') { 
-              if (window.scrollY > lastScrollY) { // if scroll down hide the navbar
-                setShow(false)
-              } else { // if scroll up show the navbar
-                if (window.scrollY > 104) {
-                  setShow(true)
-                }
-              }
-              setLastScrollY(window.scrollY)
-            }
-          }
-  
-        if (typeof window !== 'undefined') {
-            window.addEventListener('scroll', controlNavbar)
-
-            return () => {
-                window.removeEventListener('scroll', controlNavbar)
-            }
-        }
-    }, [lastScrollY])
-
-    const clickHandle = (t) => {
-        dispatch(setType({ type: t }))
-        dispatch(setOpen({ open: true }))
-    }
-
-    const navRight = () => {
-        if (!role) {
-            return (
-                <nav className="main-header__nav">
-                    <button className="login__button" onClick={() => clickHandle(MODAL.TYPE.SignIn)}>
-                        Sign In</button>
-                    <button className="signup__button" onClick={() => clickHandle(MODAL.TYPE.SignUp)}>
-                        Sign Up</button>
-                </nav>
-            )
-        }
-
-        return (
-            <nav className="main-header__nav">
-                { REGEX.ROUTES.WRITE.test(pathname)
-                    ? <PublishButton />
-                    : <button className="login__button" onClick={() => navigate('/write')}>Write</button>
-                }
-                <button className="login__button" onClick={sendLogout}>Log off</button>
-            </nav>
-        )
-    }
-
+    
     return (
         <>
             <Modal />
-            <header className={`main-header__container ${show 
-                    && 'main-header-scroll'}`}>
-                <Link to='/'>
-                    <h1 className="main-header__title">devspot</h1>
-                </Link>
-                {navRight()}
-            </header>
+            <Header showState={[show, setShow]} />
             <div className='main__container'>
                 <Outlet context={[show, setShow]} />
             </div>
