5b61681374803bbff4735f52fbfb2bf4740bcd8f
Jian yong Lei <jianyonglei@gmail.com>
2023-06-09 15:02:44 -0400

    Upadted upload route 200 res code

--- backend/routes/uploadRoute.js
+++ backend/routes/uploadRoute.js
@@ -5,6 +5,7 @@ const upload = require('../config/multerConfig')
 router.route('/').post((req, res) => {
     upload(req, res, (err) => {
         if (err) res.status(400).json({ message: 'Bad request - either file size is too large or format is not accepted' })
+        else res.status(200).json({ message: 'File uploaded successfully' })
     })
 })
 
